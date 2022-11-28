008606fa9ede6a45e7f99107a4f41589de2afb0e
Julieta Gordillo <julietagordillo13@hotmail.com>
2022-11-28 00:01:55 -0300

    corrijo itemlistcontainer

--- Documents/Julieta/Coderhouse/React/practica/my-app/src/App.jsx
+++ Documents/Julieta/Coderhouse/React/practica/my-app/src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
 import Products from './components/Products';
 
 function App() {
--- Documents/Julieta/Coderhouse/React/practica/my-app/src/components/ItemListContainer.jsx
+++ Documents/Julieta/Coderhouse/React/practica/my-app/src/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ItemListContainer = () => ({ greeting }) => {
+const ItemListContainer = ({ greeting }) => {
     return (
         <div className="container py-5">
             <div className="row">
